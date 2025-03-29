e69cac83adca1a0d67453fba937b0745d256ada6
KarthikO1109-1999 <obaleshkarthik@gmail.com>
2025-03-29 12:58:40 +0530

    Working App for 50 Score

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
-'use strict';
+"use strict";
 
-const player0E1 = document.querySelector('.player--0');
-const player1E1 = document.querySelector('.player--1');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 // Select item by ID
-const score0E1 = document.querySelector('#score--0');
-const score1E1 = document.querySelector('#score--0');
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--0");
 // Select item by ID
-const current0E1 = document.querySelector('#current--0');
-const current1E1 = document.querySelector('#current--1');
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
 
-const btnRoll = document.querySelector('.btn--roll');
-const btnhold = document.querySelector('.btn--hold');
-const btnNewGame = document.querySelector('.btn--new');
-const diceE1 = document.querySelector('.dice');
-
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const btnNewGame = document.querySelector(".btn--new");
+const diceEl = document.querySelector(".dice");
 
 //starting condition
-score0E1.textContent = 0;
-score1E1.textContent = 0;
-diceE1.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+let playing = true;
 
 let currentScore = 0;
+let activePlayer = 0;
+let scores = [0, 0];
 
-btnRoll.addEventListener('click',function(){
-//  Generating random values for dices
-const dice = Math.trunc((Math.random() * 6 )+ 1);
-console.log(dice);
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
 
-//Remove hidden change file
-diceE1.classList.remove('hidden');
-diceE1.src = `dice-${dice}.png`;
+  // This make if class is there list it removes or else it adds
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
-if(dice !== 1){
-    currentScore += dice;
-    current0E1.textContent = currentScore;
-}else{
-    console.log('Switch Player 2')
-}
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    //  Generating random values for dices
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    console.log(dice);
 
-
-
+    //Remove hidden change file
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
 });
 
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 50) {
+      playing =false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});

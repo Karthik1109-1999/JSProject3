f9e8e1122f11ee52ed8e7079954275d72136890e
KarthikO1109-1999 <obaleshkarthik@gmail.com>
2025-03-29 15:06:19 +0530

    Added Callback Init function to NewGame

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 // Select item by ID
 const score0El = document.querySelector("#score--0");
-const score1El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
 // Select item by ID
 const current0El = document.querySelector("#current--0");
 const current1El = document.querySelector("#current--1");
@@ -14,6 +14,22 @@ const btnHold = document.querySelector(".btn--hold");
 const btnNewGame = document.querySelector(".btn--new");
 const diceEl = document.querySelector(".dice");
 
+const init = function(){
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -78,3 +94,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNewGame.addEventListener('click',init);
